476a00cb1d68703825a11c1597232b896c59dd60
cztamas <tamas.czank@gmail.com>
2016-11-01 21:40:37 +0100

    ...

---
+++ TúraKezelő/DAOs/DBHandler.cs
@@ -0,0 +1,26 @@
+﻿using HikeHandler.DAOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace TúraKezelő.DAOs
+{
+    public class DBHandler
+    {
+        private CountryDao countryDao;
+        private RegionDao regionDao;
+        private CPDao cpDao;
+        private HikeDao hikeDao;
+
+        public DBHandler(MySqlConnection connection)
+        {
+            countryDao = new CountryDao(connection);
+            regionDao = new RegionDao(connection);
+            cpDao = new CPDao(connection);
+            hikeDao = new HikeDao(connection);
+        }
+    }
+}

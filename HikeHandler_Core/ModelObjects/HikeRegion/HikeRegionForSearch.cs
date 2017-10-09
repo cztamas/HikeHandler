4144ccf34d97ba011ebaacc39a8e116225a55dcf
cztamas <tamas.czank@gmail.com>
2017-10-09 23:06:51 +0200

    RegionDao changes partly done

--- HikeHandler_Core/ModelObjects/HikeRegion/HikeRegionForSearch.cs
+++ HikeHandler_Core/ModelObjects/HikeRegion/HikeRegionForSearch.cs
@@ -4,14 +4,15 @@ namespace HikeHandler.ModelObjects
 {
     public struct HikeRegionForSearch
     {
-        public string name, countryName;
+        public string name, countryName, description;
         public IntPile hikeCount, cpCount;
         public int? countryID;
 
         [JsonConstructor]
-        public HikeRegionForSearch(string name, string countryName, IntPile hikeCount, IntPile cpCount, int? countryID = null)
+        public HikeRegionForSearch(string name, string description, string countryName, IntPile hikeCount, IntPile cpCount, int? countryID = null)
         {
             this.name = name;
+            this.description = description;
             this.countryName = countryName;
             this.hikeCount = hikeCount;
             this.cpCount = cpCount;
--- HikeHandler_Data/DAOs/RegionDao.cs
+++ HikeHandler_Data/DAOs/RegionDao.cs
@@ -18,67 +18,121 @@ namespace HikeHandler.DAOs
 
         private string GetSearchCommand(HikeRegionForSearch template)
         {
-            string commandText = @"SELECT r.idregion, r.name, r.hikecount, r.cpcount, r.description,
- c.name AS countryname, c.idcountry FROM region r, country c 
-WHERE r.idcountry=c.idcountry AND r.name LIKE @name AND c.name LIKE @cname";
-            if (template.HikeCount != null)
+            string commandText = @"SELECT r.regionID, r.name, r.hikecount, r.cpcount, r.description,
+ c.name AS countryname, c.countryID FROM region r, country c, region_country rc 
+WHERE r.regionID = rc.regionID AND c.countryID = rc.countryID 
+AND r.name LIKE @name AND c.name LIKE @cname AND r.description LIKE @description";
+            if (template.hikeCount != null)
             {
-                string countCondition = template.HikeCount.SqlSearchCondition("region.hikecount");
+                string countCondition = template.hikeCount.SqlSearchCondition("r.hikecount");
                 if (countCondition != String.Empty)
                     commandText += (" AND " + countCondition);
             }
-            if (template.IDcountry != null)
+            if (template.cpCount != null)
+            {
+                string countCondition = template.cpCount.SqlSearchCondition("r.cpcount");
+                if (countCondition != String.Empty)
+                    commandText += (" AND " + countCondition);
+            }
+            if (template.countryID != null)
                 commandText += (" AND c.idcountry=@idcountry");
-            if (template.IDRegion != null)
-                commandText += (" AND r.idregion=@idregion");
             commandText += " ORDER BY name ASC;";
             return commandText;
         }
 
         public List<HikeRegionForView> SearchRegion(HikeRegionForSearch template)
         {
-            if (sqlConnection == null)
-            {
-                throw new NoDBConnectionException();
-            }
-            if (sqlConnection.State != ConnectionState.Open)
+            if (sqlConnection == null || sqlConnection.State != ConnectionState.Open)
             {
                 throw new NoDBConnectionException();
             }
             string commandText = GetSearchCommand(template);
-            List<HikeRegionForView> resultList = new List<HikeRegionForView>();
             using (MySqlCommand command = new MySqlCommand(commandText, sqlConnection))
             {
-                command.Parameters.AddWithValue("@name", "%" + template.Name + "%");
-                command.Parameters.AddWithValue("@cname", "%" + template.CountryName + "%");
-                if (template.IDcountry != null)
-                    command.Parameters.AddWithValue("@idcountry", template.IDcountry);
-                if (template.IDRegion != null)
-                    command.Parameters.AddWithValue("@idregion", template.IDRegion);
+                command.Parameters.AddWithValue("@name", "%" + template.name + "%");
+                command.Parameters.AddWithValue("@cname", "%" + template.countryName + "%");
+                command.Parameters.AddWithValue("@description", "%" + template.description + "%");
+                if (template.countryID != null)
+                    command.Parameters.AddWithValue("@idcountry", template.countryID);
                 using (MySqlDataReader reader = command.ExecuteReader())
                 {
-                    if (reader.HasRows)
+                    if (!reader.HasRows)
                     {
-                        while(reader.Read())
+                        return new List<HikeRegionForView>();
+                    }
+                    var dictionary = new Dictionary<int, HikeRegionForView>();
+                    while(reader.Read())
+                    {
+                        int regionID = reader.GetInt32("regionID");
+                        int countryID = reader.GetInt32("countryID");
+                        string name = reader.GetString("name");
+                        int hikeCount = reader.GetInt32("hikecount");
+                        int cpCount = reader.GetInt32("cpcount");
+                        string countryName = reader.GetString("countryname");
+                        string description = reader.GetString("description");
+                        HikeRegionForView item;
+                        if (dictionary.TryGetValue(regionID, out item))
                         {
-                            int regionID = reader.GetInt32("idregion");
-                            int countryID = reader.GetInt32("idcountry");
-                            string name = reader.GetString("name");
-                            int hikeCount = reader.GetInt32("hikecount");
-                            int cpCount = reader.GetInt32("cpcount");
-                            string countryname = reader.GetString("countryname");
-                            string description = reader.GetString("description");
-                            resultList.Add(new HikeRegionForView(regionID, 
-                                countryID, name, countryname, hikeCount, cpCount, description));
+                            item.countryIDs.Add(countryID);
+                            item.countryNames.Add(countryName);
+                        }
+                        else
+                        {
+                            HikeRegionForView newRegion = new HikeRegionForView(
+                                regionID, name, new List<int> { countryID }, new List<string> { countryName }, hikeCount, cpCount, description);
+                            dictionary.Add(regionID, newRegion);
                         }
                     }
-                    else
-                        return resultList;
+                    return new List<HikeRegionForView>(dictionary.Values);
                 }
-                return resultList;
             }
         }
         
+        public HikeRegionForView GetRegionByID(int regionID)
+        {
+            if (regionID <= 0)
+            {
+                throw new ArgumentException("regionID parameter should be positive.", "regionID");
+            }
+            if (sqlConnection == null || sqlConnection.State != ConnectionState.Open)
+            {
+                throw new NoDBConnectionException();
+            }
+            string commandText =
+                @"SELECT r.name, r.description, r.hikecount, r.cpcount, c.countryID, c.name AS countryname
+FROM region r, region_country rc, country c 
+WHERE r.regionID = @regionID AND rc.regionID = r.regionID AND rc.countryID = c.countryID;";
+            using (MySqlCommand command = new MySqlCommand(commandText, sqlConnection))
+            {
+                command.Parameters.AddWithValue("@regionID", regionID);
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    if (!reader.HasRows)
+                    {
+                        throw new NoItemFoundException();
+                    }
+                    var countryIDs = new List<int>();
+                    var countryNames = new List<string>();
+                    int hikeCount = 0;
+                    int cpCount = 0;
+                    string name = String.Empty;
+                    string description = String.Empty;
+                    while (reader.Read())
+                    {
+                        int countryID = reader.GetInt32("countryID");
+                        countryIDs.Add(countryID);
+                        string countryName = reader.GetString("countryname");
+                        countryNames.Add(countryName);
+                        name = reader.GetString("name");
+                        description = reader.GetString("description");
+                        hikeCount = reader.GetInt32("hikecount");
+                        cpCount = reader.GetInt32("cpcount");
+                    }
+                    return new HikeRegionForView(regionID, name, countryIDs, countryNames, hikeCount, cpCount, description);
+                }
+            }
+        }
+
         // Recalculates the hike count of every region in the DB.
         // Only for correcting erroneous data in the DB.
         public void RecalculateRegionData()
